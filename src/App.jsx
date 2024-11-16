1644de95f7a3bacd88710b9291e1ec5bb4567c59
priteshshah96 <priteshshahwork@gmail.com>
2024-11-15 22:44:49 -0500

    app,jsx updated for sigin page error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { ClerkProvider, SignIn, SignUp, SignedIn, SignedOut } from '@clerk/clerk-react';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import { AuthProvider } from './components/AuthProvider';
 import UserDashboard from './pages/UserDashboard';
 import UserAnnotationDashboard from './pages/UserAnnotationDashboard';
@@ -19,42 +19,72 @@ function ClerkProviderWithRoutes() {
       publishableKey={clerkPubKey}
       navigate={(to) => navigate(to)}
     >
-      <SignedOut>
-        <Routes>
-          <Route 
-            path="/sign-in/*" 
-            element={<SignIn 
-              routing="path" 
-              path="/sign-in"
-              signUpUrl="/sign-up"
-              afterSignInUrl="/"
-            />} 
-          />
-          <Route 
-            path="/sign-up/*" 
-            element={<SignUp 
-              routing="path" 
-              path="/sign-up"
-              signInUrl="/sign-in"
-              afterSignUpUrl="/"
-            />} 
-          />
-          {/* Redirect to sign-in for any other routes when signed out */}
-          <Route path="*" element={<Navigate to="/sign-in" replace />} />
-        </Routes>
-      </SignedOut>
+      <Routes>
+        <Route 
+          path="/sign-in/*" 
+          element={
+            <SignedOut>
+              <SignIn 
+                routing="path" 
+                path="/sign-in"
+                signUpUrl="/sign-up"
+                afterSignInUrl="/"
+              />
+            </SignedOut>
+          } 
+        />
+        <Route 
+          path="/sign-up/*" 
+          element={
+            <SignedOut>
+              <SignUp 
+                routing="path" 
+                path="/sign-up"
+                signInUrl="/sign-in"
+                afterSignUpUrl="/"
+              />
+            </SignedOut>
+          } 
+        />
+        
+        {/* Protected Routes */}
+        <Route
+          path="/"
+          element={
+            <SignedIn>
+              <AuthProvider>
+                <UserDashboard />
+              </AuthProvider>
+            </SignedIn>
+          }
+        />
+        <Route
+          path="/file/:fileId"
+          element={
+            <SignedIn>
+              <AuthProvider>
+                <UserAnnotationDashboard mode="view" />
+              </AuthProvider>
+            </SignedIn>
+          }
+        />
+        <Route
+          path="/annotate/:fileId"
+          element={
+            <SignedIn>
+              <AuthProvider>
+                <UserAnnotationDashboard mode="edit" />
+              </AuthProvider>
+            </SignedIn>
+          }
+        />
 
-      <SignedIn>
-        <AuthProvider>
-          <Routes>
-            <Route path="/" element={<UserDashboard />} />
-            <Route path="/file/:fileId" element={<UserAnnotationDashboard mode="view" />} />
-            <Route path="/annotate/:fileId" element={<UserAnnotationDashboard mode="edit" />} />
-            {/* Redirect to dashboard for undefined routes when signed in */}
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </AuthProvider>
-      </SignedIn>
+        {/* Catch-all redirect */}
+        <Route 
+          path="*" 
+          element={<Navigate to="/sign-in" replace />} 
+        />
+      </Routes>
     </ClerkProvider>
   );
 }
