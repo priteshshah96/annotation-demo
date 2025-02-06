83b87e25869912ac1b2ecc984ced6aba99dedd7d
priteshshah96 <priteshshahwork@gmail.com>
2025-02-06 01:08:32 -0500

    Removed useSnackbarhook and moved notification to toast with multiple notification support- fixed extra spacing in showtoast

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -33,7 +33,7 @@ const UserDashboard = () => {
   const [selectedFileId, setSelectedFileId] = useState(null);
   const [menuAnchorEl, setMenuAnchorEl] = useState(null);
   const [showDocs, setShowDocs] = useState(false);
-  const [toasts, setToasts] = useState(null);
+  const [toasts, setToasts] = useState([]);  
 
    // Toast handlers
    const showToast = (message, type = 'error') => {
